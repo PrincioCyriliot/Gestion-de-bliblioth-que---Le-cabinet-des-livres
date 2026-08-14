7b77d1ad4bd1471f2de7895ac236677e78206c73
RAFALIMAEVA Princio <prafalimaeva@gmail.com>
2026-08-14 22:26:24 +0300

    Enhance logout.php with comments for session logic
    Added comments in French for clarity on session handling.

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
+session_start();//démarre la dession
 session_unset();
-session_destroy();
-header('Location: login.php');
+session_destroy();//ferme la sessiom
+header('Location: login.php');//redirection vers la page principale
 exit;
