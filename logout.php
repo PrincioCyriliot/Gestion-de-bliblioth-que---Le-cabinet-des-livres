ae1ed0fdf03e8ab9afb259589cecc58ed81d037c
RAFALIMAEVA Princio <prafalimaeva@gmail.com>
2026-08-14 22:26:37 +0300

    Fix session handling and comments in logout.php
    Corrected comments and fixed session destruction message.

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-session_start();//démarre la dession
+session_start();//démarre la session
 session_unset();
 session_destroy();//ferme la sessiom
 header('Location: login.php');//redirection vers la page principale
